40d3fbf80c39aa47f6a1a676f231861065891f3b
vattanac <vattanacsim99@gmail.com>
2018-08-26 18:29:18 +0700

    "push"

--- src/code.ts
+++ src/code.ts
@@ -6,20 +6,44 @@
 // console.log(Rx);
 
 import { Observable } from "rxjs";
-var observable = Observable.create((observer:any)=>{
-    observer.next('Hey Honey come here!')
-    observer.next('How are you?')
-    observer.complete()
-    observer.next('It will not show')
+
+var observable = Observable.create((observer: any) => {
+    try {
+
+        observer.next('Hey Honey come here!')
+        observer.next('How are you?')
+
+        setInterval(() => {
+            observer.next('I am Good');
+        },2000)
+
+
+        // observer.complete()
+        // observer.next('It will not show')
+
+    } catch (err) {
+        observer.error(err);
+    }
 });
-observable.subscribe(
-    (x:any)=> console.log(addItem(x)),
-    (error:any)=> console.log(addItem(error)),
-    ()=> console.log(addItem('Completed!!')),
+
+var observer = observable.subscribe(
+    (x: any) => console.log(addItem(x)),
+    (error: any) => console.log(addItem(error)),
+    () => console.log(addItem('Completed!!')),
 
 );
 
-function addItem(val:any) {
+var observer2 = observable.subscribe(
+    (x: any) => console.log(addItem(x)),
+);
+observer.add(observer2)
+
+setTimeout(()=>{
+    observer.unsubscribe();
+},6001)
+
+
+function addItem(val: any) {
     var node = document.createElement("li");
     var textnode = document.createTextNode(val);
     node.appendChild(textnode);
